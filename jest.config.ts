b6087ee5450b3545aa103b67e1b7321cc76a1f3c
kutakutat <taktaktak24@gmail.com>
2023-08-02 15:52:58 +0900

    feat: Add code coverage configuration to jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
   roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
